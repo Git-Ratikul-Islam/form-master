89f6b0abb53c0acab956e55970be10444e621bf6
Ratikulislam <joyofficial80@gmail.com>
2023-09-21 11:37:29 +0600

    two-different-type-form-value-created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import SimpleForm from './Components/SimpleForm/SimpleForm';
+import StateFulForm from './Components/StateFulForm/StateFulForm';
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      <StateFulForm></StateFulForm>
+
+
+
+
+
+    </>
+  );
+}
+
+export default App;
