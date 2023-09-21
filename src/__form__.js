991311c244bdcbdb743d2e05aa2c06b557983fc5
Ratikulislam <joyofficial80@gmail.com>
2023-09-21 12:02:03 +0600

    uncontrolled-element-useRef-created

--- src/__form__.js
+++ src/__form__.js
@@ -1,4 +1,11 @@
 /* 
 *1. use form tag and name to get values
+*From e access e.target.[name of the input field].value
+*
+*2.controlled element: use individual field state for each field
+*
+*3. controlled element: one object in a state holding all input fields value
+*
+*4. uncontrolled element: useRef to create a reference to the element and access value by using like: nameRef.current.value
 *
 */
